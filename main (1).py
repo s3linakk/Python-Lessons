ebf0799333f8989033ab595f94da0b9298647968
s3linakk <s3linakk@gmail.com>
2024-10-08 17:17:31 +0300

    does your triangle exist
    calculates if a triangle can exist.

---
+++ main (1).py
@@ -0,0 +1,10 @@
+print("what is your triangle's first side?")
+a=int(input())
+print("what is your triangle's second side?")
+b=int(input())
+print("what about the third side?")
+c=int(input())
+if a+b>c and a+c>b and c+b>a:
+    print("your tiangle officially existsss!!")
+else:
+    print("NOOO YOUR TRIANGLE DOESEN'T EXIIIISTTT")
